10288a091f207f6a498f4222a4ec56ce561e6cfb
Younes Cheikh <younes.cheikh@gmail.com>
2021-02-28 22:45:23 +0100

    Updated Author and Uri links

--- Source/Umator.Plugins.QRCode/PluginAssemblyInfo.cs
+++ Source/Umator.Plugins.QRCode/PluginAssemblyInfo.cs
@@ -2,8 +2,8 @@
 
 [assembly: UmatorPlugin(
     "A2041A09-6799-4174-80B2-2863A4EC8101",
-    "QRCode",
+    "Umator.QRCode",
     "Plugin to generate QR Code automatically from a given data string to a given output image file",
-    Author = "Younes CHEIKH", AuthorUri = "https://somecode.NET", DocumentationUri = "https://umator-qrcode.github.io",
-    SupportUri = "https://github.com/YounesCheikh/Umator.QRCode/issues"
+    Author = "Umator", AuthorUri = "https://umator.com", DocumentationUri = "https://umator.com/docs/umator-qrcode/",
+    SupportUri = "https://umator.com/support/forum/?view=forum&id=9"
 )]
